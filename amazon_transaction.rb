35b8d34cd10aa9c8f63a73b3cd9478812102316a
gcollins1317 <gracecollins1000@gmail.com>
2018-07-16 13:10:56 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -5,7 +5,7 @@ class AmazonTransaction
    @items = items 
    @discount = discount
  end 
- def add_item(title, price)
+ def add_item(title, price = 0.98)
    @total = price.to_i + total 
  end 
 end 
