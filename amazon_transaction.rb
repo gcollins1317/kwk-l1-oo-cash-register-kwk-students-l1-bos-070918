99530bac3e820b2f25a02aac44c0787c9ec2d3e7
gcollins1317 <gracecollins1000@gmail.com>
2018-07-16 13:05:50 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,7 +1,8 @@
 class AmazonTransaction 
- attr_accessor :total, :items
- def initialize(total = 0, items  = [])
+ attr_accessor :total, :items, :discount
+ def initialize(total = 0, items  = [], discount)
    @total = total 
    @items = items 
+   @discount = discount 
  end 
 end 
